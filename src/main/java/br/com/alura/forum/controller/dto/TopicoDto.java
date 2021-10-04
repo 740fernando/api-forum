f0b8d992f3f6d221fcb33445afa7f0fe38db1f75
Fernando Luiz <fernando.souzavieira@everis.nttdata.com>
2021-10-04 13:40:11 -0300

    Finalizado Endpoints e DTO

---
+++ src/main/java/br/com/alura/forum/controller/dto/TopicoDto.java
@@ -0,0 +1,44 @@
+package br.com.alura.forum.controller.dto;
+
+import br.com.alura.forum.modelo.Topico;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * não é uma boa prática retornar entidades JPA nos métodos dos controllers, sendo mais indicado retornar
+ * classes que seguem o padrão DTO (Data Transfer Object);
+ */
+//Consigo controlar quais campos quero devolver, porque nem sempre eu quero devolver tudo que tem na
+//minha classe de domínio.
+@Data
+public class TopicoDto {
+
+    private Long id;
+    private String titulo;
+    private String mensagem;
+    private LocalDateTime dataCriacao;
+
+    public TopicoDto(Topico topico){
+        this.id=topico.getId();
+        this.titulo=topico.getTitulo();
+        this.mensagem=topico.getMensagem();
+        this.dataCriacao=topico.getDataCriacao();
+
+    }
+
+    public static List<TopicoDto> converter(List<Topico> topicos) {
+        return  topicos.stream().map(TopicoDto::new).collect(Collectors.toList());
+    }
+}
+/**
+ * .stream().map(TopicoDto::new).collect(Collectors.toList())->Essa é a sintaxe do Java 8. Sem ele, teríamos que
+ * pegar essa lista de tópicos, fazer um for para cada tópico, dar new no topicoDto, guardar em uma lista de
+ * topicoDto e devolver essa lista de topicoDto no final. Desse jeito, ele faz tudo isso em uma linha só usando
+ * API de strings do Java 8.
+ *A função do mapeamento será TopicoDto::new, porque ele vai chamar o construtor que recebe o próprio tópico
+ * como parâmetro. No final, tenho que transformar isso em uma lista, então vou encadear a chamada para o método
+ * collect(), passando collectors.toList() para transformar numa lista.
+ */
